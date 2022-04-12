c4b933477081132fd4e2cee78196eaa631413b6a
Nusret <89344065+nusrte@users.noreply.github.com>
2022-04-12 11:39:21 +0400

    Update language.py

--- userbot/language.py
+++ userbot/language.py
@@ -1,10 +1,4 @@
-# Copyright (C) 2020 Yusuf Usta.
-#
-# Licensed under the GPL-3.0 License;
-# you may not use this file except in compliance with the License.
-#
 
-# Asena UserBot - Yusuf Usta
 
 from . import LANGUAGE, LOGS, bot, PLUGIN_CHANNEL_ID
 from json import loads, JSONDecodeError
@@ -12,11 +6,11 @@ from os import path, remove
 from telethon.tl.types import InputMessagesFilterDocument
 
 pchannel = bot.get_entity(PLUGIN_CHANNEL_ID)
-LOGS.info("Dil dosyası yükleniyor...")
+LOGS.info("Dil faylı yüklenir...")
 LANGUAGE_JSON = None
 
 for dil in bot.iter_messages(pchannel, filter=InputMessagesFilterDocument):
-    if ((len(dil.file.name.split(".")) >= 2) and (dil.file.name.split(".")[1] == "asenajson")):
+    if ((len(dil.file.name.split(".")) >= 2) and (dil.file.name.split(".")[1] == "yarasajson")):
         if path.isfile(f"./userbot/language/{dil.file.name}"):
             try:
                 LANGUAGE_JSON = loads(open(f"./userbot/language/{dil.file.name}", "r").read())
@@ -24,36 +18,36 @@ for dil in bot.iter_messages(pchannel, filter=InputMessagesFilterDocument):
                 dil.delete()
                 remove(f"./userbot/language/{dil.file.name}")
 
-                if path.isfile("./userbot/language/DEFAULT.asenajson"):
-                    LOGS.warn("Varsayılan dil dosyası kullanılıyor...")
-                    LANGUAGE_JSON = loads(open(f"./userbot/language/DEFAULT.asenajson", "r").read())
+                if path.isfile("./userbot/language/DEFAULT.yarasajson"):
+                    LOGS.warn("Varsayılan dil faylı işledilir...")
+                    LANGUAGE_JSON = loads(open(f"./userbot/language/DEFAULT.yarasajson", "r").read())
                 else:
-                    raise Exception("Your language file is invalid")
+                    raise Exception("Dil faylı sehfdir.")
         else:
             try:
                 DOSYA = dil.download_media(file="./userbot/language/")
                 LANGUAGE_JSON = loads(open(DOSYA, "r").read())
             except JSONDecodeError:
                 dil.delete()
-                if path.isfile("./userbot/language/DEFAULT.asenajson"):
-                    LOGS.warn("Varsayılan dil dosyası kullanılıyor...")
-                    LANGUAGE_JSON = loads(open(f"./userbot/language/DEFAULT.asenajson", "r").read())
+                if path.isfile("./userbot/language/DEFAULT.yarasajson"):
+                    LOGS.warn("Varsayıl dil faylı işledilir...")
+                    LANGUAGE_JSON = loads(open(f"./userbot/language/DEFAULT.yarasajson", "r").read())
                 else:
-                    raise Exception("Your language file is invalid")
+                    raise Exception("Dil faylı sehfdir.")
         break
 
 if LANGUAGE_JSON == None:
-    if path.isfile(f"./userbot/language/{LANGUAGE}.asenajson"):
+    if path.isfile(f"./userbot/language/{LANGUAGE}.yarasajson"):
         try:
-            LANGUAGE_JSON = loads(open(f"./userbot/language/{LANGUAGE}.asenajson", "r").read())
+            LANGUAGE_JSON = loads(open(f"./userbot/language/{LANGUAGE}.yarasajson", "r").read())
         except JSONDecodeError:
-            raise Exception("Invalid json file")
+            raise Exception("Sehf json faylı")
     else:
-        if path.isfile("./userbot/language/DEFAULT.asenajson"):
-            LOGS.warn("Varsayılan dil dosyası kullanılıyor...")
-            LANGUAGE_JSON = loads(open(f"./userbot/language/DEFAULT.asenajson", "r").read())
+        if path.isfile("./userbot/language/DEFAULT.yarasajson"):
+            LOGS.warn("Varsayılan dil faylı işledilir...")
+            LANGUAGE_JSON = loads(open(f"./userbot/language/DEFAULT.yarasajson", "r").read())
         else:
-            raise Exception(f"Didn't find {LANGUAGE} file")
+            raise Exception(f"{LANGUAGE} faylı tapılmadı")
 
 LOGS.info(f"{LANGUAGE_JSON['LANGUAGE']} dili yüklendi.")
 
@@ -61,12 +55,12 @@ def get_value (plugin = None, value = None):
     global LANGUAGE_JSON
 
     if LANGUAGE_JSON == None:
-        raise Exception("Please load language file first")
+        raise Exception("İlk önce dil faylını yükleyin")
     else:
         if not plugin == None or value == None:
             Plugin = LANGUAGE_JSON.get("STRINGS").get(plugin)
             if Plugin == None:
-                raise Exception("Invalid plugin")
+                raise Exception("Sehf plugin")
             else:
                 String = LANGUAGE_JSON.get("STRINGS").get(plugin).get(value)
                 if String == None:
