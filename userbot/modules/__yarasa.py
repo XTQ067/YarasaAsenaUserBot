b8b3425bb59be11b5881a1211976be4323d311bd
Nusret <89344065+nusrte@users.noreply.github.com>
2022-04-12 12:00:28 +0400

    Update and rename __asena.py to __yarasa.py

--- userbot/modules/__yarasa.py
+++ userbot/modules/__yarasa.py
@@ -1,29 +1,15 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
-# you may not use this file except in compliance with the License.
-#
-
-# Asena UserBot - Yusuf Usta
-#
-
-""" UserBot yardım komutu """
-
 from userbot.cmdhelp import CmdHelp
 from userbot import cmdhelp
 from userbot import CMD_HELP
 from userbot.events import register
 
-# ██████ LANGUAGE CONSTANTS ██████ #
-
 from userbot.language import get_value
-LANG = get_value("__asena")
+LANG = get_value("__yarasa")
 
-# ████████████████████████████████ #
-
-@register(outgoing=True, pattern="^.asena(?: |$)(.*)")
-async def asena(event):
-    """ .asena komutu için """
+@register(outgoing=True, pattern="^.yarasa(?: |$)(.*)")
+@register(outgoing=True, pattern="^.bat(?: |$)(.*)")
+async def yarasa(event):
+    """ Dilvuran Vüqar. """
     args = event.pattern_match.group(1).lower()
     if args:
         if args in CMD_HELP:
@@ -35,12 +21,12 @@ async def asena(event):
         sayfa = [sorted(list(CMD_HELP))[i:i + 5] for i in range(0, len(sorted(list(CMD_HELP))), 5)]
         
         for i in sayfa:
-            string += f'`▶️ `'
+            string += f'`✨ `'
             for sira, a in enumerate(i):
-                string += "`" + str(a)
+                string += "__" + str(a)
                 if sira == i.index(i[-1]):
-                    string += "`"
+                    string += "__"
                 else:
-                    string += "`, "
+                    string += "__, "
             string += "\n"
         await event.edit(LANG["NEED_MODULE"] + '\n\n' + string)
